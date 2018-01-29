98f396164206b0619f3681337d969f723936e4f4
Mohan S <kingsleydavid.s@thestreet.com>
2018-01-29 11:16:23 +0530

    Base version

---
+++ src/test/java/Test1212/Demo7.java
@@ -0,0 +1,12 @@
+package Test1212;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi kingsley, how are you");
+
+	}
+
+}
